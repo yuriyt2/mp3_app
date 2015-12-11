4910e95c610a59949cba5791f69d8ba8100f7340
Yuriy Turetskiy <Yuriy.Turetskiy@gmail.com>
2015-12-11 17:02:20 -0500

    syncing with github

---
+++ models/user.js
@@ -0,0 +1,48 @@
+console.log("User.js");
+
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+var UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  token: String,
+  songs: [{
+    filename: String,
+    tempUrl: String,
+    artist: String,
+    title: String,
+    album: String,
+    year: String
+  }]
+});
+
+UserSchema.pre('save', function (next) {
+  var currentUser = this;
+
+  //only hash the password if it has been modified
+  if(!currentUser.isModified('password')) return next();
+  bcrypt.genSalt(15, function (err, salt) {
+    if(err) return next();
+
+  //hash the password using bcypt
+    bcrypt.hash(currentUser.password, salt, function (err, hashedPassword) {
+      if(err) return next();
+
+  //replace the user's password with the hashed password
+      currentUser.password = hashedPassword;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function (userPassword, callback) {
+  bcrypt.compare(userPassword, this.password, function (err, isMatch) {
+    if(err) return callback(err);
+    callback(null, isMatch);
+  });
+} ;
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,120 @@
+var express = require('express');
+var logger = require('morgan');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var User = require('models/user.js');
+var mongoose = require('mongoose');
+var app = express();
+
+
+app.use(logger('dev'));
+app.use(express.static('./public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'music app secret',
+  saveUninitialized: false,
+  resave: false
+}));
+
+//establish mongo connection
+mongoose.connect('mongodb://localhost/mp3App', function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Mongo Connection Successful');
+  }
+});
+
+//set up port to run on
+app.listen(3000,function(){console.log("Server Running on 3000")});
+
+
+// app.get('/user', function(res,req){
+//   req.sendfile(__dirname + "/public/login.html")
+// })
+
+
+// app.get('/users', function(req, res) {
+//     User.find().populate('posts').exec(function(err, users) {
+//       res.send(users);
+//     });
+//   });
+//
+//   var restrictAccess = function (req, res, next) {
+//     var sessionId = req.session.currentUser;
+//     var reqId = req.params.id;
+//     sessionId = reqId ? next() : res.status(400).send({err: 400, msg: "You shall not pass"});
+//   };
+//
+//   var authenticate = function (req, res, next) {
+//     req.session.currentUser ? next() : res.status(403).send({err: 403, msg: "log in troll"});
+//   };
+//
+//   app.get('/users/:id', authenticate, restrictAccess, function (req, res) {
+//     User.findById(req.params.id).exec(function (err, user) {
+//       res.send(user);
+//     });
+//   });
+//
+  app.post('/users', function(req,res){
+    console.log(req.body)
+    console.log("hi")
+    var user = new User(req.body);
+    User.findOne({
+      username: req.body.username
+    }).exec(function(err,results){
+      if (results !== null){
+        res.send('User already exists.')
+      }else if (req.body.password.length < 5){
+         res.send('Password should be at least 5 letters long.')}
+      else {user.save(function(err) {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("User saved");
+                res.send("Account created. Please log in.");
+              }
+            });
+          }
+      })
+  })
+//
+//   app.put('/users', function (req, res) {
+//     console.log("put route session id : " + req.session.currentUser);
+//     User.findById(req.session.currentUser).populate('posts').exec(function (err, user) {
+//       console.log(user)
+//       user.addPost(req, res)
+//     });
+//   });
+
+app.post('/sessions', function(req,res){
+
+  User.find({
+     username: req.body.username
+   }).exec(function(err, user) {
+     console.log(user)
+     if (user[0] === undefined){
+     res.send('User not found.')
+     console.log(req.session)}
+     else{
+     user[0].comparePassword(req.body.password, function(err, isMatch) {
+       if (isMatch) {
+         console.log("logged in");
+         console.log(user[0]._id)
+         req.session.currentUser = user[0]._id;
+         res.send(user);
+         console.log(req.session)
+       } else {
+         res.send('Incorrect password.');
+         }
+       });
+     };
+   });
+});
+
+app.delete('/sessions', function(req,res){
+  req.session.currentUser = null;
+  res.send(console.log('logged out'))
+  res.send(console.log(req.session.currentUser))
+})
